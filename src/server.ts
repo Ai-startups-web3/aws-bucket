2423fb6cd07a0ffcaca7448330bb4b9b6b1bd125
shubhamkunwar10 <fonders@metakul.com>
2025-03-24 13:38:02 +0530

    update port name

--- src/server.ts
+++ src/server.ts
@@ -11,5 +11,5 @@ app.use(express.json());
 
 app.use("/api", routes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.AWS_BUCKET_PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
